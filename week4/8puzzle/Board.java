99eeab01d463b795c99a0af0e0256c3349396b18
toljio <toljio@gmail.com>
2020-12-14 21:57:18 +0300

    week4 puzzle board

--- week4/8puzzle/Board.java
+++ week4/8puzzle/Board.java
@@ -1,9 +1,10 @@
+import edu.princeton.cs.algs4.Queue;
+
 import java.util.Arrays;
-import java.util.Iterator;
 
 public final class Board {
     private final int[][] board;
-    private int n;
+    private final int n;
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
@@ -11,19 +12,15 @@ public final class Board {
         if (n1 < 2 || n1 >= 128) throw new IllegalArgumentException("2 ≤ n < 128");
         int n2 = tiles[0].length;
         if (n2 != n1) throw new IllegalArgumentException("input matrix should be squire");
-//        int check = 0;
-//        for (int i =0; i < n1; i++) {
-//            for (int j=0; j < n2; j++) {
-//                check |= 1<<tiles[i][j];
-//            }
-//        }
-//        System.out.println(Math.pow(2, 127*127));
-//        if (check != Math.pow(2, n1*n1) - 1) throw new IllegalArgumentException();
-        board = tiles.clone();
         n = n1;
+        board = new int[n][];
+        for (int i = 0; i < n; i++) {
+            board[i] = tiles[i].clone();
+        }
     }
 
     // string representation of this board
+    @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append(n);
@@ -31,7 +28,7 @@ public final class Board {
         for (int i = 0; i < n; i++) {
             StringBuilder builder2 = new StringBuilder();
             for (int v : board[i]) {
-                builder2.append(Integer.toString(v));
+                builder2.append(" " + v);
             }
             builder.append(builder2);
             if (i != n - 1) builder.append("\n");
@@ -64,9 +61,9 @@ public final class Board {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 0) continue;
-                int row_diff = Math.abs(i - (board[i][j] - 1) / n);
-                int col_diff = Math.abs(j - (board[i][j] - 1) % n);
-                k += row_diff + col_diff;
+                int rowDiff = Math.abs(i - (board[i][j] - 1) / n);
+                int colDiff = Math.abs(j - (board[i][j] - 1) % n);
+                k += rowDiff + colDiff;
             }
         }
         return k;
@@ -79,90 +76,101 @@ public final class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return toString().equals(y.toString());
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) {
+            return false;
+        }
+        Board that = (Board) y;
+        if (!Arrays.deepEquals(that.board, board)) return false;
+        return true;
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return new Iterable<Board>() {
-
-            @Override
-            public Iterator<Board> iterator() { return new BoardIterator(); }
-            class BoardIterator implements Iterator<Board> {
-                private int row0;
-                private int col0;
-                private boolean leftSeen = false;
-                private boolean rightSeen = false;
-                private boolean topSeen = false;
-                private boolean botSeen = false;
-                public BoardIterator() {
-                    for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < n; j++) {
-                            if (board[i][j] == 0) {
-                                row0 = i;
-                                col0 = j;
-                                break;
-                            }
-                        }
-                    }
-                    if (row0 == 0) {
-                        topSeen = true;
-                    }
-                    if (row0 == n-1) {
-                        botSeen = true;
-                    }
-                    if (col0 == 0) {
-                        leftSeen = true;
-                    }
-                    if (col0 == n-1) {
-                        rightSeen = true;
-                    }
-                }
-
-                @Override
-
-                public boolean hasNext() {
-                    return !(topSeen && botSeen && leftSeen && rightSeen);
-                }
-
-                @Override
-                public Board next() {
-                    int[][] bro = new int[n][];
-                    for (int i = 0; i < n; i++) {
-                        bro[i] = board[i].clone();
-                    }
-                    int row0New = row0;
-                    int col0New = col0;
-                    if (!topSeen) {
-                        row0New -= 1;
-                        topSeen = true;
-                    } else if (!botSeen) {
-                        row0New += 1;
-                        botSeen = true;
-                    } else if (!leftSeen) {
-                        col0New -= 1;
-                        leftSeen = true;
-                    } else if (!rightSeen) {
-                        col0New += 1;
-                        rightSeen = true;
-                    }
-                    bro[row0][col0] = bro[row0New][col0New];
-                    bro[row0New][col0New] = 0;
-                    return new Board(bro);
+        Queue<Board> bros = new Queue<>();
+        int row0 = 0;
+        int col0 = 0;
+        boolean leftSeen = false;
+        boolean rightSeen = false;
+        boolean topSeen = false;
+        boolean botSeen = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row0 = i;
+                    col0 = j;
+                    break;
                 }
             }
-        };
+        }
+        if (row0 == 0) {
+            topSeen = true;
+        }
+        if (row0 == n-1) {
+            botSeen = true;
+        }
+        if (col0 == 0) {
+            leftSeen = true;
+        }
+        if (col0 == n-1) {
+            rightSeen = true;
+        }
+        while (!(topSeen && botSeen && leftSeen && rightSeen)) {
+            int[][] bro = new int[n][];
+            for (int i = 0; i < n; i++) {
+                bro[i] = board[i].clone();
+            }
+            int row0New = row0;
+            int col0New = col0;
+            if (!topSeen) {
+                row0New -= 1;
+                topSeen = true;
+            } else if (!botSeen) {
+                row0New += 1;
+                botSeen = true;
+            } else if (!leftSeen) {
+                col0New -= 1;
+                leftSeen = true;
+            } else if (!rightSeen) {
+                col0New += 1;
+                rightSeen = true;
+            }
+            bro[row0][col0] = bro[row0New][col0New];
+            bro[row0New][col0New] = 0;
+            bros.enqueue(new Board(bro));
+        }
+        return bros;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return neighbors().iterator().next();
+        int[][] bro = new int[n][];
+        for (int i = 0; i < n; i++) {
+            bro[i] = board[i].clone();
+        }
+        if (bro[0][0] != 0) {
+            if (bro[0][1] != 0) {
+                int tmp = bro[0][0];
+                bro[0][0] = bro[0][1];
+                bro[0][1] = tmp;
+            } else if (bro[1][0] != 0) {
+                int tmp = bro[0][0];
+                bro[0][0] = bro[1][0];
+                bro[1][0] = tmp;
+            }
+        } else {
+            int tmp = bro[0][1];
+            bro[0][1] = bro[1][1];
+            bro[1][1] = tmp;
+        }
+        return new Board(bro);
     }
 
     // unit testing (not graded)
     public static void main(String[] args) {
-        int[][] a = {{8,1,3},{4,0,2},{7,6,5}};
-        int[][] b = {{8,1,3},{4,0,2},{7,5,6}};
+        int[][] a = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
+        int[][] b = {{8, 1, 3}, {4, 0, 2}, {7, 5, 6}};
         Board test = new Board(a);
         Board test2 = new Board(b);
         System.out.println(test.toString());
@@ -171,8 +179,8 @@ public final class Board {
         System.out.println(test.isGoal());
         System.out.println(test.dimension());
         System.out.println(test.equals(test2));
-//        test2 = test.twin();
-//        System.out.println(test2.toString());
+        test2 = test.twin();
+        System.out.println("twin " + test2.toString());
         System.out.println(test.toString());
 
         for (Board brd : test.neighbors()) {
---
+++ week4/8puzzle/Solver.java
@@ -0,0 +1,104 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Solver {
+    private boolean solvable;
+    private SearchNode lastNode;
+
+    private static class SearchNode implements Comparable<SearchNode> {
+        private final Board board;
+        private final int moves;
+        private final SearchNode prev;
+
+        public SearchNode(Board board, int moves, SearchNode prev) {
+            this.board = board;
+            this.moves = moves;
+            this.prev = prev;
+        }
+        public int compareTo(SearchNode that) {
+            return this.moves + this.board.manhattan() - (that.moves + that.board.manhattan());
+        }
+    }
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        if (initial == null) throw new IllegalArgumentException();
+        SearchNode initSearchNode = new SearchNode(initial, 0, null);
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        pq.insert(initSearchNode);
+        MinPQ<SearchNode> pq2 = new MinPQ<>();
+        pq2.insert(new SearchNode(initial.twin(), 0, null));
+        SearchNode minSearchNode = pq.delMin();
+        SearchNode minSearchNode2 = pq2.delMin();
+        while (!minSearchNode.board.isGoal() && !minSearchNode2.board.isGoal()) {
+            for (Board b: minSearchNode.board.neighbors()) {
+                if (minSearchNode.prev == null || !b.equals(minSearchNode.prev.board)) {
+                    SearchNode cur = new SearchNode(b, minSearchNode.moves + 1, minSearchNode);
+                    pq.insert(cur);
+                }
+            }
+            minSearchNode = pq.delMin();
+            for (Board b: minSearchNode2.board.neighbors()) {
+                if (minSearchNode2.prev == null || !b.equals(minSearchNode2.prev.board)) {
+                    SearchNode cur = new SearchNode(b, minSearchNode2.moves + 1, minSearchNode2);
+                    pq2.insert(cur);
+                }
+            }
+            minSearchNode2 = pq2.delMin();
+        }
+        if (minSearchNode.board.isGoal()) {
+            solvable = true;
+            lastNode = minSearchNode;
+        } else {
+            solvable = false;
+            lastNode = null;
+        }
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        if (!isSolvable()) return -1;
+        return lastNode.moves;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        if (!isSolvable()) return null;
+        SearchNode cur = lastNode;
+        Stack<Board> way = new Stack<>();
+        while (cur != null) {
+            way.push(cur.board);
+            cur = cur.prev;
+        }
+        return way;
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+}
